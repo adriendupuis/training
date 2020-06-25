ec990feff8e69308a2671c736c3329916b6921e5
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-25 16:34:14 +0200

    SearchController w/ PagerFanta

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\Core\Pagination\Pagerfanta\ContentSearchAdapter;
 use eZ\Publish\Core\QueryType\QueryTypeRegistry;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -44,4 +46,32 @@ class SearchController extends AbstractController
 
         ));
     }
+
+    public function bikeRidePaginatedSearch(Request $request): Response
+    {
+        $searchText = $request->get('q');
+
+        $query = $this->queryTypeRegistry->getQueryType('BikeRideList')->getQuery([
+            'contentType' => 'bike_ride',
+            'searchText' => $searchText,
+            'count' => true,
+        ]);
+
+        $pager = new Pagerfanta(
+            new ContentSearchAdapter($query, $this->searchService)
+        );
+        $currentPage = $request->get('page');
+
+        $pager->setMaxPerPage(3);
+        $pager->setCurrentPage(min(max($currentPage, 1), $pager->getNbPages()));
+
+        return ($this->render(
+            '@ezdesign/search_pagination.html.twig',
+            [
+                'bike_rides' => $pager,
+                'search_text' => $searchText
+            ]
+        ));
+    }
+
 }
